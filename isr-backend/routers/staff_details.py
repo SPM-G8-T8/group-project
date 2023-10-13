8d57bbfd1988bbd3ce752564cebd740afa962add
wowmiir <syamiir@solana.fm>
2023-10-13 12:54:55 +0800

    add in new StaffSkillsUpdate api

--- isr-backend/routers/staff_details.py
+++ isr-backend/routers/staff_details.py
@@ -4,7 +4,7 @@ from fastapi_pagination.ext.sqlalchemy import paginate
 from database import get_db
 from sqlalchemy.orm import Session
 from typing import Annotated, List
-from schemas import StaffRolesRead, StaffDetailsBase, StaffSkillsRead
+from schemas import StaffRolesRead, StaffDetailsBase, StaffSkillsRead, StaffSkillsUpdate
 import models
 
 router = APIRouter()
@@ -34,9 +34,18 @@ def get_staff_details(staff_email: str, db: db_dependency):
         raise HTTPException(status_code=404, detail="Staff details not found")
     return staff_details
 
-# @router.get("/staff/all", response_model=StaffDetailsBase)
-# def get_all_staff_details(db: db_dependency):
-#     staff_details = db.query(models.StaffDetails).all()
-#     if not staff_details:
-#         raise HTTPException(status_code=404, detail="Staff details not found")
-#     return staff_details
+@router.put("/staff-skills/update/{staff_id}", response_model=List[StaffSkillsUpdate])
+def update_staff_skills(staff_id: int, updatedSkills: StaffSkillsUpdate, db: db_dependency):
+    try:
+        staff_skills = db.query(models.StaffSkills).join(models.SkillDetails).filter(models.StaffSkills.staff_id == staff_id)
+        if not staff_skills:
+            raise HTTPException(status_code=404, detail="Staff skills not found")
+        db_staff_skills.staff_id = updatedSkills.staff_id
+        db_staff_skills.skill_id = updatedSkills.skill_id
+        db_staff_skills.ss_status = updatedSkills.ss_status
+        db_staff_skills.skill = updatedSkills.skill
+        db.commit()
+        return {"message": "Staff Skill updated", "skill": jsonable_encoder(db_staff_skills)}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
