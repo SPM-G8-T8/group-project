7ccb76994af3c590cbe6db113f6d2c1b5f481a36
wowmiir <syamiir@solana.fm>
2023-10-13 12:55:16 +0800

    add in UpdateStaffSkills api router

--- isr-frontend/src/api/api.js
+++ isr-frontend/src/api/api.js
@@ -3,7 +3,7 @@ const rootURL = "http://localhost:8000";
 export const getRoleListing = rootURL + "/listing/";
 export const createRoleListing = rootURL + "/listing/create/";
 export const editRoleListing = rootURL + "/listing/edit/";
-
+ 
 export const getSkills = rootURL + "/skills/";
 export const createSkills = rootURL + "/skills/create/";
 
@@ -17,3 +17,4 @@ export const createRoleApplication = rootURL + "/applicants/create/";
 
 export const getStaffDetails = rootURL + "/staff/";
 export const getStaffSkills = rootURL + "/staff-skills/";
+export const UpdateStaffSkills = rootURL + "/staff-skills/update/";
